dafc29b8c6c89a411a3b87ee219931924dba91f6
Loganathan R <41115255+Logubabu@users.noreply.github.com>
2023-03-13 10:38:28 +0530

    initial commit

--- src/components/voter_data_display/voter_data_display.tsx
+++ src/components/voter_data_display/voter_data_display.tsx
@@ -1,28 +1,20 @@
 import React, { FC, useEffect, useState } from 'react';
-import styles from './voter_data_display.module.css';
-import { DataGrid } from '@mui/x-data-grid';
-import Box from '@mui/material/Box';
-// import { useDemoData } from '@mui/x-data-grid-generator';
-
-
 
 function VoterDataDisplay(data:any){
-  // debugger
- const [datas,setData]=useState<any>([])
+ const [voterdata,setData]=useState<any>([])
  const [tempdata,setTempdata]=useState<any>([])
  const [change,setChange]=useState(false)
 
 
 
   useEffect(() => {
-    async function fetchData() {
-      const res = await fetch("http://localhost:4000/voter_data");
-      const resp = await res.json();
-      setData(resp);
-      setTempdata(resp)
-      // return resp
-    }
-    fetchData();
+    // async function fetchData() {
+    //   const res = await fetch("http://localhost:4000/voter_data");
+    //   const resp = await res.json();
+      setData(data);
+      setTempdata(data)
+    // }
+    // fetchData();
   }, []);
   
   function UpdateVoter(){
@@ -45,7 +37,6 @@ function VoterDataDisplay(data:any){
   <label>Voter ID: <input type="text" disabled value={tempdata.voterId} className="mt-1"
   onChange={(e)=>{
     setTempdata({...tempdata,voterId:e.target.value})
-
   }}
   /></label>
   <br />
@@ -53,14 +44,12 @@ function VoterDataDisplay(data:any){
   onChange={(e)=>{
     setChange(true)
     setTempdata({...tempdata,name:e.target.value})
-
   }}/></label>
   <br />
   <label>Gender: <input type="text" value={tempdata.gender} className="mt-1"
   onChange={(e)=>{
     setChange(true)
     setTempdata({...tempdata,gender:e.target.value})
-
   }}
   /></label>
   <br />
@@ -68,78 +57,66 @@ function VoterDataDisplay(data:any){
   onChange={(e)=>{
     setChange(true)
     setTempdata({...tempdata,age:e.target.value})
-
   }}/></label>
   <br />
   <label>Phone Number: <input type="text" value={tempdata.phoneNumber} className="mt-1"
   onChange={(e)=>{
     setChange(true)
     setTempdata({...tempdata,phoneNumber:e.target.value})
-
   }}/></label>
   <br />
   <label>House Number: <input type="text" value={tempdata.houseNumber} className="mt-1"
   onChange={(e)=>{
     setChange(true)
     setTempdata({...tempdata,houseNumber:e.target.value})
-
   }}/></label>
   <br />
   <label>Street: <input type="text" value={tempdata.street} className="mt-1"
   onChange={(e)=>{
     setChange(true)
     setTempdata({...tempdata,street:e.target.value})
-
   }}/></label>
   <br />
   <label>Ward Number: <input type="text" value={tempdata.wardNumber} className="mt-1"
   onChange={(e)=>{
     setChange(true)
     setTempdata({...tempdata,wardNumber:e.target.value})
-
   }}/></label>
   <br />
   <label>Booth Number: <input type="text" value={tempdata.boothNumber} className="mt-1"
   onChange={(e)=>{
     setChange(true)
     setTempdata({...tempdata,boothNumber:e.target.value})
-
   }}/></label>
   <br />
   <label>Constituency Number: <input type="text" value={tempdata.constituencyNumber} className="mt-1"
   onChange={(e)=>{
     setChange(true)
     setTempdata({...tempdata,constituencyNumber:e.target.value})
-
   }}/></label>
   <br />
   <label>District Number: <input type="text" value={tempdata.districtNumber} className="mt-1"
   onChange={(e)=>{
     setChange(true)
     setTempdata({...tempdata,districtNumber:e.target.value})
-
   }}/></label>
   <br />
-  {/* <label>AC Booth Number: <input type="text" value={tempdata.acBoothNumber} className="mt-1"/></label>
-  <br /> */}
   <label>Serial Number: <input type="text" value={tempdata.serialNumber} className="mt-1"
   onChange={(e)=>{
     setChange(true)
     setTempdata({...tempdata,serialNumber:e.target.value})
-
   }}/></label>
   <br />
   <label>Section Number: <input type="text" value={tempdata.sectionNumber} className="mt-1"
   onChange={(e)=>{
     setChange(true)
     setTempdata({...tempdata,sectionNumbersectionNumber:e.target.value})
-
   }}/></label>
   <br />
   {change?(
     <>
     <button className="cancel-button" onClick={(e)=>{
-      setTempdata({...datas})
+      setTempdata({...voterdata})
       setChange(false)
     }}>Cancel</button>
     <button className="save-button" onClick={UpdateVoter}>Save</button>
--- src/components/voter_search/voter_search.tsx
+++ src/components/voter_search/voter_search.tsx
@@ -1,28 +1,23 @@
 import React, { FC, useEffect, useState } from 'react';
-import axios from "axios";
-import styles from './voter_search.module.css';
-import VoterDataDisplay from '../voter_data_display/voter_data_display';
+
 
 function VoterSearch(props:any) {
 
-  let [voter_data, setvoter] = useState(<></>);
+  const [voter_data, setvoter] = useState([]);
 
-  // function fetch() {
-  //   return new Promise(resolve => setTimeout(() => resolve(42), 1000));
-  // }
-  // const response =  axios.request({
-  //   baseURL: "http://localhost:4000/voter_data",
-  //   // url,
-  //   // method,
-  //   // data,
-  // });
-
-  // useEffect(() => {
-  function fetchvoterData(param:any) {
-      const res=fetch("http://localhost:4000/voter_data/?param="+param).then((resp) => resp.json());
-      console.log(res)
-      return res
+   useEffect(() => {
+    async function fetchData() {
+      const res = await fetch("http://localhost:4000/voter_data");
+      const resp = await res.json();
+      setvoter(resp);
     }
+    fetchData();
+  }, []);
+  // function fetchvoterData(param:any) {
+  //     const res=fetch("http://localhost:4000/voter_data/?param="+param).then((resp) => resp.json());
+  //     console.log(res)
+  //     return res
+  //   }
   return(
     <div className="voter_id_input border sborder-solid">
       <form>
@@ -34,9 +29,7 @@ function VoterSearch(props:any) {
         </label>
         <br/>
         <button className="search-button rounded-md border sborder-solid border-teal-500 bg-green-400">Search</button>
-        {voter_data}
-        {/* onClick={() => fetchvoterData()} */}
-        {/* <input type="submit" value="Submit" /> */}
+        {/* {voter_data} */}
       </form>
       </div>
   )
